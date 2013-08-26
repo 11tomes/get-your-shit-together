6c00c9d2ede1ac928d927ac2be464be3498feb85
Dixie Philamerah J. Atay <dixie.atay@gmail.com>
2013-08-26 11:54:34 +0800

    merge scaffold and frontend default layout

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -1,59 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="{{ asset('assets/css/bootstrap.no-icons.min.css') }}" rel="stylesheet">
-		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-		{{-- <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet"> --}}
-		<link href="{{ asset('assets/css/Handlee.css') }}" rel="stylesheet">
-		<style>
-			body, h1 { font-family: 'Handlee', cursive !important; font-weight: 400; }
-			.handwritten a { color: black !important; }
-		</style>
-		@section('styles')
-		@show
-	</head>
+@extends('frontend/layouts/default')
 
-	<body>
-		@section('navigation')
-		<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand handwritten" href="http://www.cavemag.com/10-things-you-can-learn-from-suits/">Get Your Sh*t Together</a>
-				</div>
+@section('styles')
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+{{-- <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet"> --}}
+@stop
 
-				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li>{{ link_to_route('todos.index', 'Today') }}</li>
-						<li>{{ link_to_route('todos.labels', 'Labels') }}</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		@show
-
-		<div class="container">
-			@if (Session::has('alert'))
-				<?php list($type, $message) = Session::get('alert'); ?>
-				<div class="alert alert-{{{ $type }}} alert-dismissable">
-					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-					{{{ $message }}}
-				</div>
-			@endif
-
-			@yield('main')
-		</div>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-		@section('scripts')
-		@show
-	</body>
-
-</html>
+@section('content')
+@if (Session::has('alert'))
+	<?php list($type, $message) = Session::get('alert'); ?>
+	<div class="alert alert-{{{ $type }}} alert-dismissable">
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		{{{ $message }}}
+	</div>
+@endif
+	@yield('main')
+@stop
