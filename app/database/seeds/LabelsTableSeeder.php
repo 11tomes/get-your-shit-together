eb360e02f227f3681cf03046068763b931263ac6
Leonel L. Tomes <mr.leonel.tomes@gmail.com>
2013-08-26 15:26:46 +0800

    update db seeder to reflect the changes in priorities and labels

--- app/database/seeds/LabelsTableSeeder.php
+++ app/database/seeds/LabelsTableSeeder.php
@@ -7,10 +7,10 @@ class LabelsTableSeeder extends Seeder {
 		DB::table('labels')->delete();
 
 		$labels = array(
-			array('label' => 'Work', 'color' => 'ee3e64', 'description' => '', 'parent_id' => '0'),
-			array('label' => 'Errands', 'color' => '4accff', 'description' => '', 'parent_id' => '0'),
-			array('label' => 'Freelance', 'color' => 'b7d84b', 'description' => '', 'parent_id' => '0'),
-			array('label' => 'Goals', 'color' => 'E2A741', 'description' => '', 'parent_id' => '0'),
+			array('name' => 'Work', 'color' => 'ee3e64', 'description' => '', 'parent_id' => '0'),
+			array('name' => 'Errands', 'color' => '4accff', 'description' => '', 'parent_id' => '0'),
+			array('name' => 'Freelance', 'color' => 'b7d84b', 'description' => '', 'parent_id' => '0'),
+			array('name' => 'Goals', 'color' => 'E2A741', 'description' => '', 'parent_id' => '0'),
 		);
 
 		 DB::table('labels')->insert($labels);
--- app/database/seeds/PrioritiesTableSeeder.php
+++ app/database/seeds/PrioritiesTableSeeder.php
@@ -7,9 +7,9 @@ class PrioritiesTableSeeder extends Seeder {
 		DB::table('priorities')->delete();
 
 		$priorities = array(
-			array('priority' => 'A', 'order' => 1, 'color' => 'ff0000', 'description' => ''),
-			array('priority' => 'B', 'order' => 2, 'color' => '00ff00', 'description' => ''),
-			array('priority' => 'C', 'order' => 3, 'color' => '0000ff', 'description' => '')
+			array('name' => 'A', 'order' => 1, 'color' => 'ff0000', 'description' => ''),
+			array('name' => 'B', 'order' => 2, 'color' => '00ff00', 'description' => ''),
+			array('name' => 'C', 'order' => 3, 'color' => '0000ff', 'description' => '')
 		);
 
 		DB::table('priorities')->insert($priorities);
