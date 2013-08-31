1f3ecba2ec003793fd4cea48b604015ca2dd70cc
Leonel L. Tomes <mr.leonel.tomes@gmail.com>
2013-08-31 23:19:02 +0800

    add logout under profile; add timezone hidden value when signing up

--- app/routes.php
+++ app/routes.php
@@ -141,7 +141,7 @@ Route::get('blog/{postSlug}', array('as' => 'view-post', 'uses' => 'BlogControll
 Route::post('blog/{postSlug}', 'BlogController@postView');
 
 */
-Route::get('/', array('as' => 'home', 'uses' => 'BlogController@getIndex'));
+Route::get('/', array('as' => 'home', 'uses' => 'HomeController@showWelcome'));
 
 Route::group(array('prefix' => 'settings'), function()
 {
--- app/views/frontend/navigation.blade.php
+++ app/views/frontend/navigation.blade.php
@@ -4,10 +4,10 @@
 			<i class="icon-eye-open"></i> View Todos <b class="caret"></b>
 		</a>
 		<ul class="dropdown-menu">
-			<li><a href="{{ route('todos.index') }}"> As List</a></li>
-			<li><a href="{{ route('todos.agenda') }}"> Agenda</a></li>
-			<li><a href="{{ route('todos.priorities') }}"> By Priorities</a></li>
-			<li><a href="{{ route('todos.labels') }}"> By Labels</a></li>
+			<li><a href="{{ route('todos.index') }}">As List</a></li>
+			<li><a href="{{ route('todos.agenda') }}">Agenda</a></li>
+			<li><a href="{{ route('todos.priorities') }}">By Priorities</a></li>
+			<li><a href="{{ route('todos.labels') }}">By Labels</a></li>
 		</ul>
 	</li>
 	<li class="dropdown">
@@ -15,8 +15,8 @@
 			<i class="icon-cog"></i> Settings <b class="caret"></b>
 		</a>
 		<ul class="dropdown-menu">
-			<li><a href="{{ route('settings.labels.index') }}"> Labels</a></li>
-			<li><a href="{{ route('settings.priorities.index') }}"> Priorities</a></li>
+			<li><a href="{{ route('settings.labels.index') }}">Labels</a></li>
+			<li><a href="{{ route('settings.priorities.index') }}">Priorities</a></li>
 		</ul>
 	</li>
 	<li class="dropdown">
@@ -27,6 +27,7 @@
 			<li><a href="{{ URL::route('profile') }}">Profile</a></li>
 			<li><a href="{{ URL::route('change-password') }}">Change Password</a></li>
 			<li><a href="{{ URL::route('change-email') }}">Change Email</a></li>
+			<li><a href="{{ URL::route('logout') }}">Logout</a></li>
 		</ul>
 	</li>
 </ul>
