56c0a89a8b983635128ee6fed66746552fa4517e
Leonel L. Tomes <mr.leonel.tomes@gmail.com>
2013-09-05 13:51:47 +0800

    fix error notification during signin

--- app/views/frontend/auth/signin.blade.php
+++ app/views/frontend/auth/signin.blade.php
@@ -16,11 +16,13 @@ Account Sign in ::
 
 {{-- Page content --}}
 @section('content')
-<form method="post" action="{{ route('signin') }}" autocomplete="off" class="form-signin">
+<form method="post" action="{{ route('signin') }}" class="form-signin">
 	<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 	<h2 class="form-signin-heading">Please sign in</h2>
 	<input type="text" class="form-control" name="email" id="email" placeholder="Email address" autofocus="" value="{{ Input::old('email') }}">
+	{{ $errors->first('email', '<span class="help-block">:message</span>') }}
 	<input type="password" class="form-control" name="password" id="password" placeholder="Password">
+	{{ $errors->first('password', '<span class="help-block">:message</span>') }}
 	<label class="checkbox">
 		<input type="checkbox" name="remember-me" id="remember-me" value="1"> Remember me
 	</label>
